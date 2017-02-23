e355df138bb6604d97eb9689f7e39250337fd862
MelodyLC <dearonion@foxmail.com>
2017-02-22 22:55:50 -0800

    retrieve friend list from DB to show on my friend page

--- routes/friends.js
+++ routes/friends.js
@@ -1,8 +1,25 @@
 /*
  * GET home page.
  */
-var data = require("../myfriends.json");
+var models = require("../models");
+//var data = require("../myfriends.json");
 
 exports.view = function(req, res){
-  res.render('myfriends', {'Friends': data});
+
+    var userID = req.session.user._id;
+    console.log("USer Id: ", userID);
+    
+    var nowTime = new Date();
+    
+    
+    models.User
+    .find({_id: userID})
+    .populate({
+        path: '_friends'
+    })
+    .exec(function(err, usr){
+        if (err) return res.send(500);
+        console.log(usr[0]._friends);
+        res.render('myfriends', {'Friends':usr[0]._friends});
+        });
 };
